c25810450615ebd57122628655bb6062b37e0294
Abhishek Kumar <abhishekku@iitbhilai.ac.in>
2023-10-19 16:59:22 +0530

    home_page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import "../styles/home.css"
+import AboutUS from './AboutUs'
 const Home = () => {
     return(
-        <div>Home</div>
+        <>
+        <div className='Home_page'>
+            <img src="./images/logo.png" alt="logo"/>
+            <h1>INGENUITY</h1>
+            <h3>The Official Programming Club of IIt Bhilai</h3>
+        </div>
+        <AboutUS />
+        </>
     )
 }
 
