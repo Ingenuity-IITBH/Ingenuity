97bddb9140e28a4941dfd0e3b92cb668948f81ee
Abhishek2310k <abhishekku@iitbhilai.ac.in>
2023-12-15 17:34:03 +0530

    changes to home page and about Us page

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -5,9 +5,11 @@ const aboutUS = () => {
         <>
         <div className='aboutUs'>
             <div className='main_content'>
-                <h2>About us()</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <h2>About us</h2>
+                <p>Welcome to Ingenuity, the official competitive programming club of IIT Bhilai, where innovation meets algorithmic brilliance! Established with the vision to foster a culture of problem-solving and intellectual curiosity, Ingenuity is the hub for passionate programmers and coding enthusiasts at IIT Bhilai.</p>
+                <p>Under the leadership of the dynamic coordinators, Arnav Gautam and Om Prakash Sahu, Ingenuity has become a vibrant community dedicated to honing the skills of its members in the realm of competitive programming. Arnav Gautam and Om Prakash Sahu, with their wealth of experience and deep-rooted commitment to the art of coding, serve as the driving force behind Ingenuity's success.</p>
+                <p>Ingenuity offers a platform for IIT Bhilai students to explore the fascinating world of competitive programming, participate in coding contests, and engage in collaborative learning. Through workshops, coding sessions, and regular competitions, the club strives to enhance the problem-solving abilities and algorithmic thinking of its members.</p>
+                <p>Join us at Ingenuity, where we celebrate the spirit of ingenuity, perseverance, and camaraderie. Whether you're a seasoned coder or just starting your coding journey, Ingenuity welcomes you to be a part of a community that values innovation and the pursuit of excellence in the exciting field of competitive programming. Together, let's unravel the complexities of algorithms and pave the way for a future filled with coding brilliance!</p>
             </div>
             <img src="./images/Artboard 1 1.png" alt="logo"/>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
         <div className='Home_page'>
             <img src="./images/logo.png" alt="logo"/>
             <h1>INGENUITY</h1>
-            <h3>The Official Programming Club of IIt Bhilai</h3>
+            <h3>The Official Programming Club of IIT Bhilai</h3>
         </div>
         <AboutUS />
         </>
